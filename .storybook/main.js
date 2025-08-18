afaad66d17bca8c3dcf066fb114c62eddae6d7a9
Alfie728 <zegro728@gmail.com>
2025-08-18 12:45:52 +1000

    chore: update Storybook and related dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,39 @@
 module.exports = {
   stories: ['../examples/**/*.stories.@(tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { chrome: 100, safari: 15, firefox: 91 } },
+              ],
+              '@babel/preset-typescript',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
   },
 };
--- examples/multiple_lrc/index.stories.tsx
+++ examples/multiple_lrc/index.stories.tsx
@@ -1,27 +1,23 @@
-/* eslint-disable react/function-component-definition */
-import { type ComponentStory } from '@storybook/react';
-import StaticComponent from './static';
 import { originalLrc, translatedLrc } from '../data';
 import AutoScrollComponent from './auto_scroll';
 
-export default {
+const meta = {
   title: 'MultipleLrc',
+  component: AutoScrollComponent,
 };
 
-const AutoScrollTemplate: ComponentStory<typeof AutoScrollComponent> = (
-  args,
-) => <AutoScrollComponent {...args} />;
-export const AutoScroll = AutoScrollTemplate.bind({});
-AutoScroll.args = {
-  lrcs: [originalLrc, translatedLrc],
-  recoverAutoScrollInterval: 5000,
-  verticalSpace: true,
+export default meta;
+
+export const AutoScroll = {
+  args: {
+    lrcs: [originalLrc, translatedLrc],
+    recoverAutoScrollInterval: 5000,
+    verticalSpace: true,
+  },
 };
 
-const StaticTemplate: ComponentStory<typeof StaticComponent> = (args) => (
-  <StaticComponent {...args} />
-);
-export const Static = StaticTemplate.bind({});
-Static.args = {
-  lrcs: [originalLrc, translatedLrc],
+export const Static = {
+  args: {
+    lrcs: [originalLrc, translatedLrc],
+  },
 };
